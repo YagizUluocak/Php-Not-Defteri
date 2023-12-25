63457fb41231491e326db4d107ac6b3c70928864
Yagiz <uluocak.yagiz1@gmail.com>
2023-12-25 22:41:54 +0300

    Php-Not-Defteri

---
+++ data/notlar.php
@@ -0,0 +1,31 @@
+<div class="col-8 notes-div">
+
+<h3 class="ustbaslik">NOTLARIM</h3>
+
+
+<div class="alert alert-success mt-4" role="alert">
+  <span class="badge badge-pill badge-success mb-4">Düşük Öncelik</span>
+  <h4 class="alert-heading">Başlık</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+</div>
+
+<div class="alert alert-success mt-4" role="alert">
+  <span class="badge badge-pill badge-success mb-4">Düşük Öncelik</span>
+  <h4 class="alert-heading">Başlık</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+</div>
+
+<div class="alert alert-success mt-4" role="alert">
+  <span class="badge badge-pill badge-success mb-4">Düşük Öncelik</span>
+  <h4 class="alert-heading">Başlık</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+</div>
+
+<div class="alert alert-success mt-4" role="alert">
+  <span class="badge badge-pill badge-success mb-4">Düşük Öncelik</span>
+  <h4 class="alert-heading">Başlık</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+</div>
+
+
+</div>
